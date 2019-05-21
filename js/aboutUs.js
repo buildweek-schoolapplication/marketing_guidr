3e7e872e7774f03b21389ac9c74764374861f9b2
Jamison Blackwell <blackwellj1040@gmail.com>
2019-05-21 11:39:34 -0400

    finished desktop version of aboutUs page

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -25,23 +25,10 @@ class Link {
     }
     
     toggleContent() {
-        this.detailLink.classList.toggle('change');        // const items = document.querySelectorAll('.tabs-item');
-        // // console.log(items);
-        // items.forEach(function (items) {
-        //     items.classList.remove('jamie-skills-dropdown');
-        // });
-        
-        //     items.forEach(function (items) {
-        //         items.classList.remove('jamie-about-dropdown');
-        //           })
-                //   this.link.classList.add('jamie-about-dropdown');
-        // this.detailLink.classList.add('jamie-skills-dropdown');
+        this.detailLink.classList.toggle('change');       
+       
     }
 }
-
-   
- 
-    
 
 
 
